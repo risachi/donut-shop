6c8f5eb20a8260b0f3b21a1837e69233eca1a3e4
Lisa Hackenberger <risachi@gmail.com>
2015-11-14 00:45:24 -0800

    make look beautiful

--- donutScripts.js
+++ donutScripts.js
@@ -1,4 +1,4 @@
-var donutShop = function(name, minCustomers, maxCustomers, avgDonutsPerCustomer, numHoursOpen) {
+var DonutShop = function(name, minCustomers, maxCustomers, avgDonutsPerCustomer, numHoursOpen) {
   this.shopName = name;
   this.minCustomers = minCustomers;
   this.maxCustomers = maxCustomers;
@@ -6,21 +6,21 @@ var donutShop = function(name, minCustomers, maxCustomers, avgDonutsPerCustomer,
   this.numHoursOpen = numHoursOpen;
 
   this.toTableRow = function() {
-    var tableRow = "<tr><td>" + this.shopName + "</td>";
-    tableRow += "<td>" + this.minCustomers + "</td>";
-    tableRow += "<td>" + this.maxCustomers + "</td>";
-    tableRow += "<td>" + this.avgDonutsPerCustomer + "</td>"
-    tableRow += "<td>" + this.numHoursOpen + "</td></tr>";
+    var tableRow = "<tr class='tableData'><td>" + this.shopName + "</td>";
+    tableRow += "<td class='numeric'>" + this.minCustomers + "</td>";
+    tableRow += "<td class='numeric'>" + this.maxCustomers + "</td>";
+    tableRow += "<td class='numeric'>" + this.avgDonutsPerCustomer + "</td>"
+    tableRow += "<td class='numeric'>" + this.numHoursOpen + "</td></tr>";
     return tableRow;
   }
 };
 
 var shops = [
-  new donutShop("Blue Star Donuts", 8, 43, 4.5, 11),
-  new donutShop("Voodoo Doughnuts", 4, 37, 2, 24),
-  new donutShop("Coco", 9, 23, 6.33, 11),
-  new donutShop("Tonallis Donuts & Cream", 2, 28, 1.25, 17),
-  new donutShop("Sesame Donuts", 8, 58, 3.75, 24)
+  new DonutShop("Blue Star Donuts", 8, 43, 4.5, 11),
+  new DonutShop("Voodoo Doughnuts", 4, 37, 2, 24),
+  new DonutShop("Coco", 9, 23, 6.33, 11),
+  new DonutShop("Tonallis Donuts & Cream", 2, 28, 1.25, 17),
+  new DonutShop("Sesame Donuts", 8, 58, 3.75, 24)
 ];
 
 var printToTable = function() {
