b6ba7e7d497586860f9ca506b13e4a26f51bfd40
Lisa Hackenberger <risachi@gmail.com>
2015-11-23 00:04:12 -0800

    changed jQuery to vanilla JS

--- formRefreshShop.js
+++ formRefreshShop.js
@@ -1,15 +1,29 @@
 function setupFormEvents() {
-  $('input').on('change', function(e) {
-    displayNewShopValue(e);
-  });
+  document.addEventListener("change", displayNewShopValue);
 }
 
 function displayNewShopValue(e) {
-  var el = e.target;
-  var newValue = el.value;
-  var shop = shops[ $(el).data('shop-id') ];
-  var property = $(el).data('shop-property');
+  var newValue = e.target.value;
+  var shopId = e.target.getAttribute('data-shop-id');
+  var shop = shops[ shopId ];
+  var property = e.target.getAttribute('data-shop-property');
 
   shop[property] = parseInt(newValue);
   shop.refresh();
 }
+
+// function setupFormEvents() {
+//   $('input').on('change', function(e) {
+//     displayNewShopValue(e);
+//   });
+// }
+
+// function displayNewShopValue(e) {
+//   var el = e.target;
+//   var newValue = el.value;
+//   var shop = shops[ $(el).data('shop-id') ];
+//   var property = $(el).data('shop-property');
+//
+//   shop[property] = parseInt(newValue);
+//   shop.refresh();
+// }
